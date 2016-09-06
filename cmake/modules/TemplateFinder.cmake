925688bcf4a9f276e73fd5be7b4b3b9c35504c1f
Martchus <martchus@gmx.net>
2016-09-06 22:52:12 +0200

    Fix messages in TemplateFinder.cmake

--- cmake/modules/TemplateFinder.cmake
+++ cmake/modules/TemplateFinder.cmake
@@ -4,15 +4,15 @@ if(NOT DEFINED FIND_TEMPLATE_FILE_EXISTS)
         if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/cmake/templates/${FILE_NAME}.in")
             # check own source directory
             set(${OUTPUT_VAR} "${CMAKE_CURRENT_SOURCE_DIR}/cmake/templates/${FILE_NAME}.in" PARENT_SCOPE)
-            message(STATUS "Using template for ${FILE_NAME} from own source directory.")
+            message(STATUS "Using template for ${FILE_NAME} from own (${META_PROJECT_NAME}) source directory.")
         elseif(EXISTS "${${PROJECT_VAR_NAME}_SOURCE_DIR}/cmake/templates/${FILE_NAME}.in")
             # check sources of project
             set(${OUTPUT_VAR} "${${PROJECT_VAR_NAME}_SOURCE_DIR}/cmake/templates/${FILE_NAME}.in" PARENT_SCOPE)
-            message(STATUS "Using template for ${FILE_NAME} from ${PROJECT_NAME} source directory.")
+            message(STATUS "Using template for ${FILE_NAME} from ${PROJECT_VAR_NAME} source directory.")
         elseif(EXISTS "${${PROJECT_VAR_NAME}_CONFIG_DIRS}/templates/${FILE_NAME}.in")
             # check installed version of project
             set(${OUTPUT_VAR} "${${PROJECT_VAR_NAME}_CONFIG_DIRS}/templates/${FILE_NAME}.in" PARENT_SCOPE)
-            message(STATUS "Using template for ${FILE_NAME} from ${PROJECT_NAME} installation.")
+            message(STATUS "Using template for ${FILE_NAME} from ${PROJECT_VAR_NAME} installation.")
         else()
             message(FATAL_ERROR "Template for ${FILE_NAME} file can not be located.")
         endif() 
