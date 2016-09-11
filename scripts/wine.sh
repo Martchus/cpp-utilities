5f48579cd38de226af54101e384912f057cd78ac
Martchus <martchus@gmx.net>
2016-09-11 20:30:45 +0200

    List scripts in project file
    Maybe add install target for scripts later

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+if [[ $# -lt 1 ]]; then
+    echo "Starts the specified executable with wine."
+    echo "Usage: $0 executable [additional_dll_path(s)]"
+    exit -1
+fi
 for arg in "${@:2}"; do
     WINEPATH="${arg};${WINEPATH}"
 done
 export WINEPATH;
-wine "${@:1:1}"
+echo wine "${@:1:1}"
