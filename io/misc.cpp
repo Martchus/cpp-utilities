6115933756366f2c450e8144454810c83d73a0d0
Martchus <martchus@gmx.net>
2017-02-03 00:55:15 +0100

    Add convenience function to read entire file at once

---
+++ io/misc.cpp
@@ -0,0 +1,33 @@
+#include "./misc.h"
+#include "./catchiofailure.h"
+
+#include <fstream>
+#include <streambuf>
+
+using namespace std;
+
+namespace IoUtilities {
+
+/*!
+ * \brief Reads all contents of the specified file in a single call.
+ * \throws Throws std::ios_base::failure when an error occurs or the specified \a maxSize
+ *         would be exceeded.
+ */
+string readFile(const string &path, std::string::size_type maxSize)
+{
+    ifstream file;
+    file.exceptions(ios_base::failbit | ios_base::badbit);
+    file.open(path, ios_base::in | ios_base::binary);
+    file.seekg(0, ios_base::end);
+    string res;
+    const auto size = static_cast<string::size_type>(file.tellg());
+    if(maxSize != string::npos && size > maxSize) {
+        throwIoFailure("File exceeds max size");
+    }
+    res.reserve(size);
+    file.seekg(ios_base::beg);
+    res.assign((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    return res;
+}
+
+}
---
+++ io/misc.h
@@ -0,0 +1,14 @@
+#ifndef IOUTILITIES_MISC_H
+#define IOUTILITIES_MISC_H
+
+#include "../global.h"
+
+#include <string>
+
+namespace IoUtilities {
+
+CPP_UTILITIES_EXPORT std::string readFile(const std::string &path, std::string::size_type maxSize = std::string::npos);
+
+}
+
+#endif // IOUTILITIES_MISC_H
