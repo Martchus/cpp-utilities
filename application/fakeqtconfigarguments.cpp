60e4fead05a380a2b848657a5e7e957dccdf79c6
Martchus <martchus@gmx.net>
2017-10-25 19:32:45 +0200

    Deprecate FakeQtConfigArguments

--- application/fakeqtconfigarguments.cpp
+++ application/fakeqtconfigarguments.cpp
@@ -6,6 +6,7 @@ namespace ApplicationUtilities {
  * \class FakeQtConfigArguments
  * \brief The FakeQtConfigArguments class provides arguments for the Qt GUI used when
  *        the application hasn't been built with Qt GUI support.
+ * \deprecated Get rid of this and simply don't add arguments for Qt GUI when disabled.
  */
 
 /*!
