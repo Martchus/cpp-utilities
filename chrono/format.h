46e9d4b1e4b7c3cfc3f18a07216c7e3c7344417f
Martchus <martchus@gmx.net>
2017-12-03 14:55:49 +0100

    Add formatting for Period class

--- chrono/format.h
+++ chrono/format.h
@@ -2,6 +2,7 @@
 #define CHRONO_FORMAT_H
 
 #include "./datetime.h"
+#include "./period.h"
 
 #include <ostream>
 
@@ -13,6 +14,11 @@ inline std::ostream &operator<<(std::ostream &out, const ChronoUtilities::DateTi
 inline std::ostream &operator<<(std::ostream &out, const ChronoUtilities::TimeSpan &value)
 {
     return out << value.toString(ChronoUtilities::TimeSpanOutputFormat::Normal, false);
+}
+
+inline std::ostream &operator<<(std::ostream &out, const ChronoUtilities::Period &value)
+{
+    return out << value.years() << " years, " << value.months() << " months, " << value.days() << " days";
 }
 
 #endif // CHRONO_FORMAT_H
