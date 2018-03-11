7b974a1b1ddc34e7f30a2d0787f5febf074dd414
Martchus <martchus@gmx.net>
2018-03-11 22:36:59 +0100

    Add 'const char *' overload to ConversionException

--- conversion/conversionexception.h
+++ conversion/conversionexception.h
@@ -12,8 +12,18 @@ class CPP_UTILITIES_EXPORT ConversionException : public std::runtime_error {
 public:
     ConversionException() USE_NOTHROW;
     ConversionException(const std::string &what) USE_NOTHROW;
+    ConversionException(const char *what) USE_NOTHROW;
     ~ConversionException() USE_NOTHROW;
 };
+
+/*!
+ * \brief Constructs a new ConversionException. \a what is a C-style string
+ *        describing the cause of the ConversionException.
+ */
+inline ConversionException::ConversionException(const char *what) USE_NOTHROW : std::runtime_error(what)
+{
+}
+
 } // namespace ConversionUtilities
 
 #endif // CONVERSION_UTILITIES_CONVERSIONEXCEPTION_H
