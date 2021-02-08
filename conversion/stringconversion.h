27e9761f63e137893b95588523bef7b5dc5dcc8c
Martchus <martchus@gmx.net>
2021-02-09 00:22:20 +0100

    Adjust conditions for C++20 for g++

--- conversion/stringconversion.h
+++ conversion/stringconversion.h
@@ -194,11 +194,11 @@ Container splitStringSimple(const typename Container::value_type &string, const 
         if (delimPos == Container::value_type::npos) {
             delimPos = string.size();
         }
-#if __cplusplus >= 202002
+#if __cplusplus >= 201709
         if constexpr (requires { res.emplace_back(string); }) {
 #endif
             res.emplace_back(string.substr(i, delimPos - i));
-#if __cplusplus >= 202002
+#if __cplusplus >= 201709
         } else {
             res.emplace(string.substr(i, delimPos - i));
         }
