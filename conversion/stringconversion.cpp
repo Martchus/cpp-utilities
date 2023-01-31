25e4eebb649a62b99d88578a0545ba76419cf714
Martchus <martchus@gmx.net>
2023-01-31 22:29:38 +0100

    Avoid MSVC warning about possible overflow

--- conversion/stringconversion.cpp
+++ conversion/stringconversion.cpp
@@ -255,8 +255,9 @@ WideStringData convertMultiByteToWide(std::error_code &ec, const char *inputBuff
  */
 WideStringData convertMultiByteToWide(std::error_code &ec, const std::string &inputBuffer)
 {
-    return convertMultiByteToWide(
-        ec, inputBuffer.data(), inputBuffer.size() < (std::numeric_limits<int>::max() - 1) ? static_cast<int>(inputBuffer.size() + 1) : -1);
+    return convertMultiByteToWide(ec, inputBuffer.data(), inputBuffer.size() < static_cast<std::size_t>(std::numeric_limits<int>::max() - 1)
+        ? static_cast<int>(inputBuffer.size() + 1)
+        : -1);
 }
 
 /*!
