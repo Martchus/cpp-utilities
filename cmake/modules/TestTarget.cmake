7dff72d0bd3c2d96dbaeaafd2c211992028be615
Martchus <martchus@gmx.net>
2023-10-15 16:57:29 +0200

    Apply cmake-format

--- cmake/modules/TestTarget.cmake
+++ cmake/modules/TestTarget.cmake
@@ -8,9 +8,9 @@ if (TEST_CONFIG_DONE)
 endif ()
 
 include(TestUtilities)
-if(NOT BUILD_TESTING)
+if (NOT BUILD_TESTING)
     return()
-endif()
+endif ()
 
 # find and link against CppUnit if required (used by all my projects, so it is required by default)
 if (NOT META_NO_CPP_UNIT)
